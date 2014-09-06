43b98dbda2d053beae72f9d4d3163dff0081dd37
wurenny <wurenny@163.com>
2014-09-06 09:49:58 +0800

    httpclient4

---
+++ crawler/appClientModule/com/renny/extracthtml/ExtractContext.java
@@ -0,0 +1,7 @@
+package com.renny.extracthtml;
+
+public class ExtractContext {
+	protected static final String lineSign =System.getProperty("line.separator");
+	protected static final int lineSign_size =lineSign.length();
+	
+}
